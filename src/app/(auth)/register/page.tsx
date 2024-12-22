780b35c227dc855bde9208fe0ce0f2b0bc409f81
Joseph Adeosun <mdpadeosun@yahoo.ie>
2024-12-22 21:43:12 +0000

    Add Login and Register Form Components

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RegisterForm from './RegisterForm'
 
 export default function RegisterPage() {
   return (
-    <div>
-      RegisterPage
+    <div className='flex items-center justify-center vertical-center'>
+      <RegisterForm/>
     </div>
   )
 }
